67b5cd55693d146cf1ba64af76a21e0053a24454
carsonlevine <carsonglevine@me.com>
2015-12-05 10:34:58 -0500

    added three view controllers
    contact, login, homepage

---
+++ Whistle/HomepageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomepageViewController.swift
+//  Whistle
+//
+//  Created by Carson Levine on 12/5/15.
+//  Copyright © 2015 KCC. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+}
---
+++ Whistle/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Whistle
+//
+//  Created by Carson Levine on 12/5/15.
+//  Copyright © 2015 KCC. All rights reserved.
+//
+
+import UIKit
+import DigitsKit
+
+class LoginViewController: UIViewController {
+
+}
